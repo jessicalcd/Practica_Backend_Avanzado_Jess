08b26efd63e5116e104f3e3797f2e50099632de0
jessicaluzcd <jessicaluzcd@gmail.com>
2025-03-25 12:40:35 +0000

    Añadimos middleware de errores y 404

--- nodepop/app.js
+++ nodepop/app.js
@@ -1,9 +1,26 @@
 import express from 'express'
+import createHttpError from 'http-errors'
 
 const app = express()
 
+app.use((req, res, next) => {
+    console.log('Llega petición de tipo', req.method, 'a', req.url)
+    next()
+})
+
 app.get('/', (req, res, next) => {
     res.send('Hola')
+})
+
+//catch 404 and sebd error
+app.use((req, res, next) => {
+    next(createError(404))
+})
+
+//error handler
+app.use((req, res, next) => {
+    res.status(err.status || 500)
+    res.send('Ocurrio un error: ' + err.mesage)
 })
 
 export default app
--- nodepop/server.js
+++ nodepop/server.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 3000
 
 const server = http.createServer(app)
 
+server.on('error', err => console.error(err))
 server.on('listening', () => {
     console.log(`Server started on http://localhost:${port}`)
 })
