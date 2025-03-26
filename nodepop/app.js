f0ccf78667f5702af8cfa6f46e4cb7b60a95cc2d
jessicaluzcd <jessicaluzcd@gmail.com>
2025-03-26 16:06:07 +0000

    Conectamos a MongoDB

--- nodepop/app.js
+++ nodepop/app.js
@@ -1,16 +1,28 @@
+import path from 'node:path'
 import express from 'express'
 import createError from 'http-errors'
 import logger from 'morgan'
 import * as homController from './controllers/homeController.js'
+import connectMongoose from './lib/connectMongoose.js'
+
+await connectMongoose()
+console.log('Conecte to MongoBD')
 
 const app = express()
 
 app.set('views', 'views')
-app.set('view engine', 'ejs')
+app.set('view engine', 'html')
+app.engine('html', (await import('ejs')).__express)
+ 
+app.locals.appName = 'Nodepop'
 
 app.use(logger('dev'))
+app.use(express.static(path.join(import.meta.dirname, 'public')))
+
+/* Aplication routs */
 
 app.get('/', homController.index)
+
 
 //catch 404 and sebd error
 app.use((req, res, next) => {
@@ -20,7 +32,10 @@ app.use((req, res, next) => {
 //error handler
 app.use((err, req, res, next) => {
     res.status(err.status || 500)
-    res.send('Ocurrio un error: ' + err.mesage)
+    res.locals.message = err.message
+    res.locals.error = process.env.NODEPOP_ENV === 'development' ? err : {}
+
+    res.render('error')
 })
 
 export default app
--- nodepop/controllers/homeController.js
+++ nodepop/controllers/homeController.js
@@ -1,3 +1,17 @@
 export function index (req, res, next) {
-    res.render('home', { appName: 'Nodepop' })
+    res.locals.users = [
+        {name: 'Lampara LED ajustable', price: 40 },
+        {name: 'Zapatillas Nike Air Max', price: 100 },
+        {name: 'Bicicleta Trek Marlin 7', price: 650 },
+        {name: 'iPhone 13 Pro Max', price: 1100}
+    ]
+
+    const now = new Date()
+    res.locals.esPar = (now.getSeconds() % 2 ) === 0
+    res.locals.segundoActual = now.getSeconds()
+
+    res.locals.codigo = '<script>alert("inyectado!!!")</script>'
+
+    res.render('home')
 }
+
