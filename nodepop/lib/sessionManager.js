7096530baadf7d5ab846ee649212653ea63d26db
jessicaluzcd <jessicaluzcd@gmail.com>
2025-03-27 01:48:12 +0000

    Guardamos seccion de db

--- nodepop/lib/sessionManager.js
+++ nodepop/lib/sessionManager.js
@@ -1,4 +1,5 @@
 import session from 'express-session'
+import MongoStore from 'connect-mongo'
 
 const INACTIVITY_EXPIRATION_2_DAYS = 1000 * 60 * 60 * 24 * 2 
 
@@ -8,7 +9,10 @@ export const middleware = session({
     secret: 'y`TPvm%L972=<:p(NdtE;"Q>/#Y&3a}r*RG',
     saveUninitialized: true,
     resave: false,
-    cookie:{ maxAge: INACTIVITY_EXPIRATION_2_DAYS}
+    cookie:{ maxAge: INACTIVITY_EXPIRATION_2_DAYS},
+    store: MongoStore.create({
+        mongoUrl: 'mongodb://localhost/nodepop'
+    })
 })
 
 export function useSessionInViews(req, res, next) {
