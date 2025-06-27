c921c47775850839d13c7d326f37515c1851998a
jessicaluzcd <jessicaluzcd@gmail.com>
2025-06-27 09:11:59 +0100

    Filtro por nombre

--- nodepop/controllers/homeController.js
+++ nodepop/controllers/homeController.js
@@ -4,9 +4,18 @@ import Product from '../models/Product.js'
 export async function index (req, res, next) {
     try {
         const userId = req.session.userId
-        
-        res.locals.products = await Product.find({ owner: userId})
+        const filterName = req.query.name
 
+        const filter = {
+            owner: userId,
+        }
+
+        if (filterName) {
+            filter.name = filterName
+        }
+        
+        res.locals.products = await Product.find(filter)
+ 
     const now = new Date()
     res.locals.esPar = (now.getSeconds() % 2 ) === 0
     res.locals.segundoActual = now.getSeconds()
